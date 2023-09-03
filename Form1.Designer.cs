f2acd2357cfed8f187cbaec78075dee4e7cbab7f
Stepa7936 <76559787+Stepa7936@users.noreply.github.com>
2023-09-03 13:54:29 +0300

    Рекурсия
    В данном приложения задается количество, после чего происходит рекурсия, функция вызывает саму себя.

---
+++ Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace rekurs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(15, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(96, 45);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(129, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1022, 606);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1180, 648);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rekurs
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Pen p1 = new Pen(Color.Blue, 5);
+        int c = 6;
+        int r;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            g = pictureBox1.CreateGraphics();
+            g.Clear(SystemColors.Control);
+            g.TranslateTransform(0, c);
+            draw_b(r);
+            draw(c,c);
+            draw_c(r);
+            draw(c, -c);
+            draw_d(r);
+            draw(-c, -c);
+            draw_a(r);
+            draw(-c, c);
+        }
+        private void draw (int x, int y)
+        {
+            g.DrawLine(p1, 0, 0, x, y);
+            g.TranslateTransform(x, y);
+        }
+        private void draw_b(int p)
+        {
+            if (p == 1)
+            {
+                draw(c,c);
+                draw(0,c*2);
+                draw(-c, c);
+            }
+            else
+            {
+                draw_b(p-1);
+                draw(c, c);
+                draw_c(p - 1);
+                draw(0, c * 2);
+                draw_a(p - 1);
+                draw(-c, c);
+                draw_b(p - 1);
+            }
+        }
+        private void draw_c(int p)
+        {
+            if (p == 1)
+            {
+                draw(c, -c);
+                draw(c*2, 0);
+                draw(c, c);
+            }
+            else
+            {
+                draw_c(p - 1);
+                draw(c, -c);
+                draw_d(p - 1);
+                draw(c * 2, 0);
+                draw_b(p - 1);
+                draw(c, c);
+                draw_c(p - 1);
+            }
+        }
+        private void draw_d(int p)
+        {
+            if (p == 1)
+            {
+                draw(-c,-c);
+                draw(0, -c * 2);
+                draw(c, -c);
+            }
+            else
+            {
+                draw_d(p - 1);
+                draw(-c,-c);
+                draw_a(p - 1);
+                draw(0, -c * 2);
+                draw_c(p - 1);
+                draw(c, -c);
+                draw_d(p - 1);
+            }
+        }
+        private void draw_a(int p)
+        {
+            if (p == 1)
+            {
+                draw(-c, c);
+                draw(-c * 2, 0);
+                draw(-c, -c);
+            }
+            else
+            {
+                draw_a(p - 1);
+                draw(-c, c);
+                draw_b(p - 1);
+                draw(-c * 2, 0);
+                draw_d(p - 1);
+                draw(-c, -c);
+                draw_a(p - 1);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                r = int.Parse(textBox1.Text);
+            }
+        }
+
+    }
+}
